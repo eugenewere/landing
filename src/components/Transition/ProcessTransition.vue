39bd841d2aea1fbdc3552ab23c7ce9cc403d9907
eugene were <eugenecodedev@gmail.com>
2023-12-07 15:52:09 +0300

    update on transition titles

--- src/components/Transition/ProcessTransition.vue
+++ src/components/Transition/ProcessTransition.vue
@@ -2,7 +2,7 @@
   <section id="delivery" class="bg-white dark:bg-gray-900 py-10">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="w-100 text-center  mb-8 lg:mb-16">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Deliver More, Save More.</h2>
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Make deliver more, save more bigger</h2>
         <p class="text-gray-500 sm:text-xl dark:text-gray-400">
           Maximize savings as you increase deliveries, optimizing both efficiency and cost-effectiveness.
         </p>
@@ -10,18 +10,18 @@
       <div class="space-y-8 md:grid md:grid-cols-1 lg:grid-cols-3 md:gap-12 md:space-y-0 text-center">
 
         <div class="flex items-center flex-col">
-          <h3 class="about-header">Old Way</h3>
+          <h3 class="about-header capitalize">traditional way</h3>
           <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
               <li class="flex items-center">
                   <svg aria-hidden="true" class="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-         </svg>
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                 </svg>
                   Slow Delivery
               </li>
               <li class="flex items-center">
                   <svg aria-hidden="true" class="w-3.5 h-3.5 me-2 text-gray-500 dark:text-gray-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-         </svg>
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                 </svg>
                  Spend More
               </li>
 
@@ -33,7 +33,7 @@
             <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">to</span>
         </div>
          <div class="flex items-center flex-col">
-          <h3 class="about-header">New Way</h3>
+          <h3 class="about-header capitalize"> Kebu platform</h3>
           <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
               <li class="flex items-center">
                 <svg class="flex-shrink-0 w-3.5 h-3.5 text-kebu-500 dark:text-kebu-400 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
