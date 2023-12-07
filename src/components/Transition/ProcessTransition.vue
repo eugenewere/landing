d93e372cef3842bc77c7a3082fffd69e9f9f11d3
eugene were <eugenecodedev@gmail.com>
2023-12-07 15:39:01 +0300

    fixed delivery link issue

--- src/components/Transition/ProcessTransition.vue
+++ src/components/Transition/ProcessTransition.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="about" class="bg-white dark:bg-gray-900 py-10">
+  <section id="delivery" class="bg-white dark:bg-gray-900 py-10">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="w-100 text-center  mb-8 lg:mb-16">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Deliver More, Save More.</h2>
